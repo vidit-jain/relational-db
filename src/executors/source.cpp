c15dc1d6a23e8316429de0e176ee6b4d1ec5968a
PramodRaoB <pramod.b@research.iiit.ac.in>
2023-08-17 09:59:20 +0530

    Add q1 queries and source.cpp

--- src/executors/source.cpp
+++ src/executors/source.cpp
@@ -27,8 +27,54 @@ bool semanticParseSOURCE()
     return true;
 }
 
+void doCommand_()
+{
+    logger.log("doCommand_");
+    if (syntacticParse() && semanticParse())
+        executeCommand();
+    return;
+}
+
 void executeSOURCE()
 {
     logger.log("executeSOURCE");
+    regex delim("[^\\s,]+");
+    string command;
+    string sourceFileName = "../data/" + parsedQuery.sourceFileName + ".ra";
+    fstream fin(sourceFileName, ios::in);
+
+    while(!fin.eof())
+    {        
+        tokenizedQuery.clear();
+        parsedQuery.clear();
+        logger.log("\nReading New Command: ");
+        getline(fin, command);
+        logger.log(command);
+        cout << "Executing command: " << command << endl;
+
+        auto words_begin = std::sregex_iterator(command.begin(), command.end(), delim);
+        auto words_end = std::sregex_iterator();
+        for (std::sregex_iterator i = words_begin; i != words_end; ++i)
+            tokenizedQuery.emplace_back((*i).str());
+
+        if (tokenizedQuery.size() == 1 && tokenizedQuery.front() == "QUIT")
+        {
+            break;
+        }
+
+        if (tokenizedQuery.empty())
+        {
+            continue;
+        }
+
+        if (tokenizedQuery.size() == 1)
+        {
+            cout << "SYNTAX ERROR" << endl;
+            continue;
+        }
+
+        doCommand_();
+    }
+
     return;
 }
