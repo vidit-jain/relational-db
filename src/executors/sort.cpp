398a8aabf3e28c5fda151d881619e988f456fb8d
Dheeraja7252 <dheeraja.rajreddygari@students.iiit.ac.in>
2023-10-10 03:05:32 +0530

    Implement Semantic parsing for sort

--- src/executors/sort.cpp
+++ src/executors/sort.cpp
@@ -5,9 +5,7 @@
  * 
  * syntax:
  * SORT <table_name> BY <column_name1, column_name2,..., column_namek> IN <ASC|DESC, ASC|DESC,..., ASC|DESC>
- * R <- SORT relation_name BY column_name IN sorting_order
- * 
- * sorting_order = ASC | DESC 
+ *
  */
 bool syntacticParseSORT() {
     logger.log("syntacticParseSORT");
@@ -25,8 +23,8 @@ bool syntacticParseSORT() {
     parsedQuery.queryType = SORT;
     parsedQuery.sortRelationName = tokenizedQuery[1];
     for (auto i = 0; i < numSortColumns; i++) {
-        parsedQuery.sortingStrategies.push_back((tokenizedQuery[i] == "ASC") ? ASC : DESC);
-        parsedQuery.sortColumnNames.push_back(tokenizedQuery[IN_idx + 1 + i]);
+        parsedQuery.sortingStrategies.push_back((tokenizedQuery[IN_idx + 1 + i] == "ASC") ? ASC : DESC);
+        parsedQuery.sortColumnNames.push_back(tokenizedQuery[3 + i]);
     }
     return true;
 }
@@ -34,19 +32,16 @@ bool syntacticParseSORT() {
 bool semanticParseSORT() {
     logger.log("semanticParseSORT");
 
-    if (tableCatalogue.isTable(parsedQuery.sortResultRelationName)) {
-        cout << "SEMANTIC ERROR: Resultant relation already exists" << endl;
-        return false;
-    }
-
     if (!tableCatalogue.isTable(parsedQuery.sortRelationName)) {
         cout << "SEMANTIC ERROR: Relation doesn't exist" << endl;
         return false;
     }
 
-    if (!tableCatalogue.isColumnFromTable(parsedQuery.sortColumnName, parsedQuery.sortRelationName)) {
-        cout << "SEMANTIC ERROR: Column doesn't exist in relation" << endl;
-        return false;
+    for (const auto &col: parsedQuery.sortColumnNames) {
+        if (!tableCatalogue.isColumnFromTable(col, parsedQuery.sortRelationName)) {
+            cout << "SEMANTIC ERROR: One or more columns do not exist in relation" << endl;
+            return false;
+        }
     }
 
     return true;
